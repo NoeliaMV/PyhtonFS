cecbd74941a64ff66b20c4f30b629de7665d3e23
NoeliaMV <noeliaanahi.viera@gmail.com>
2024-03-31 12:28:49 -0300

    prueba 4

--- Prueba1.py
+++ Prueba1.py
@@ -1,4 +1,5 @@
 # Imprime un mensaje en la terminal
 print("Hola, esto es un mensaje desde Python en Studio Code nueva.")
 print("linea entre medio nueva")
+print("linea entre medio nueva")
 print("segunda prueba, cambio en la linea anterior, elimino palabra y sumo otra")
