1f8d1cc57cb11f80ee75d9004ab04f07edf688bd
NoeliaMV <noeliaanahi.viera@gmail.com>
2024-03-31 12:26:19 -0300

    prueba 2 agregando cosas

--- Prueba1.py
+++ Prueba1.py
@@ -1,2 +1,3 @@
 # Imprime un mensaje en la terminal
-print("Hola, esto es un mensaje desde Python en Visual Studio Code.")
+print("Hola, esto es un mensaje desde Python en Studio Code nueva.")
+print("segunda prueba, cambio en la linea anterior, elimino palabra y sumo otra")
